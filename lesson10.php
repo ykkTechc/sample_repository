a8f5c1098a892b3d3333a472d4372d151b3776e9
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-06-20 14:35:24 +0900

    01_php_basic lesson09~11 修正

--- lesson10.php
+++ lesson10.php
@@ -1,9 +1,10 @@
 <?php
-$today = date("Y-m-d H:i:s");
-
-print_r($today);
-
-
+$w = date("w");
+$week = array("日","月","火","水","木","金","土");
+$today = date("Y年m月d日")."($week[$w]曜日)";
+echo ($today);
 ?>
-
-
+<!-- // 
+今日の日付けを以下の形式で表示してください。
+// xxxx年xx月xx日（x曜日） 
+-->
