4edd263bba7f9e5db1c686974b06931d12ef5ad1
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2022-06-24 12:31:32 +0900

    02_loop_lesson 9  修正3

--- loop_lesson09.php
+++ loop_lesson09.php
@@ -1,4 +1,5 @@
 ﻿<?php
+
 // 以下配列の中身をfor文を使用して表示してください。
 // 表示はHTMLの<table>タグを使用すること
 
@@ -14,57 +15,55 @@
  */
 
 $arr = [
-            ['a1' => 10, 'b2' => 7,  'c3' => 25],
-            ['a1' => 5,  'b2' => 8,  'c3' => 9],
-            ['a1' => 20, 'b2' => 12, 'c3' => 130],
-            ['d1' => 10, 'e2' => 5,  'f3' => 20],
-            ['d1' => 7,  'e2' => 8,  'f3' => 12],
-            ['d1' => 25, 'e2' => 9,  'f3' => 130],
-            ['g1' => 42],
-            ['g1' => 22],
-            ['g1' => 162],
-            ['g1' => 35],
-            ['g1' => 27],
-            ['g1' => 164]
+    'r1' => ['c1' => 10, 'c2' => 5, 'c3' => 20],
+    'r2' => ['c1' => 7,  'c2' => 8, 'c3' => 12],
+    'r3' => ['c1' => 25, 'c2' => 9, 'c3' => 130]
 ];
+$sum=[];
+$tbody="<tbody>\n";
+for( $i=0; $i<count($arr); $i++ ){
+    $tbody.="<tr>\n";
+    $tbody.="<th>".array_keys($arr)[$i]."</th>";
+    for($j=0;$j<count(array_values($arr)[$i]);$j++){
+        $val=array_values(array_values($arr)[$i])[$j];
+        $tbody.="<td>$val</td>";
+        if(!isset($sum[$j]))$sum[$j]=0;
+        $sum[$j]+=$val;
+    }
+    $tbody.="<td>".array_sum(array_values($arr)[$i])."</td>";
+    $tbody.="</tr>\n";
+}
+$tbody.="<tr>\n";
+$tbody.="<th>計</th>";
+for($i=0;$i<count(array_values($arr)[0]);$i++){
+    $tbody.="<td>".$sum[$i]."</td>";
+}
+$tbody.="<td>".array_sum($sum)."</td>";
+$tbody.="</tr>\n";
+
+$tbody.="</tbody>\n";
 ?>
 <!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="utf-8">
-<title>テーブル表示</title>
 <style>
 table {
     border:1px solid #000;
     border-collapse: collapse;
 }
 th, td {
-    border:1px solid #000;
+border:1px solid #000;
 }
 </style>
-</head>
-<body>
 <table>
-    <?php
-    foreach ($arr as $arr_1) {
-        $sum += $arr_1[a1];
-        $sum_1 += $arr_1[b2];
-        $sum_2 += $arr_1[c3];
-        $sum_3 += $arr_1[d1];
-        $sum_4 += $arr_1[e2];
-        $sum_5 += $arr_1[f3];
-        $sum_6 += $arr_1[g1];
-        // $sum_6 += $arr_1[a1] + $arr_1[b2] + $arr_1[c3] + $arr_1[d1] + $arr_1[e2] + $arr_1[f3];
-        // $sum_6 += $sum + $sum_1 + $sum_2 + $sum_3 + $sum_4 + $sum_5;
-    };
-    ?>
-    <tr><th>_____</th><th>_c1</th><th>_c2</th><th>_c3</th><th>横合計</th></tr>
-    <tr><td>___r1</td><td>_10</td><td>__5</td><td>_20</td><td><?php echo "___".$sum;?></td></tr>
-    <tr><td>___r2</td><td>__7</td><td>__8</td><td>_12</td><td><?php echo "___".$sum_1?></td></tr>
-    <tr><td>___r3</td><td>_25</td><td>__9</td><td>130</td><td><?php echo "__".$sum_2?></td></tr>
-    <tr><td>縦合計</td><td><?php echo "_".$sum_3?></td><td><?php echo "_".$sum_4 ?></td><td><?php echo $sum_5 ?></td><td><?php echo "__".$sum_6 ?> </td> </tr>
+<thead>
+<tr>
+<th>_____</th>
+<th>_c1</th>
+<th>_c2</th>
+<th>_c3</th>
+<th>計</th>
+</tr>
+</thead>
+<?= $tbody;?>
 </table>
-    <!-- ここにテーブル表示 -->
 </body>
-
 </html>
